c36356523efe26f4718e906e30a026a3915ba9e4
CarollCheng <carollhwrd@gmail.com>
2022-02-13 19:42:39 -0600

    header working

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,37 @@
-import { Stack, Text, Image, Persona } from '@fluentui/react'
+import { Stack, Text, Image, Persona, DefaultPalette } from '@fluentui/react'
 import logo from '../../assets/neomem.png'
 import './header.css'
 
 function Header() {
   return (
-    <Stack horizontal>
-      <Stack.Item grow>
-        <Image src={logo} alt="logo" height={50} className="logo" />
-        <Text variant="xxLarge">Neomem</Text>
-      </Stack.Item>
-      <Stack.Item>
+    <Stack
+      horizontal
+      styles={{
+        root: {
+          marginBottom: '8px',
+          padding: '4px',
+          background: DefaultPalette.black,
+        },
+      }}
+    >
+      <Stack horizontal grow>
+        <Stack.Item align="center">
+          <Stack horizontal>
+            <Stack.Item align="center">
+              <Image src={logo} alt="logo" height={44} />
+            </Stack.Item>
+            <Stack.Item align="center">
+              <Text
+                variant="xxLarge"
+                styles={{ root: { color: '#eee', fontWeight: 'normal' } }}
+              >
+                Neomem
+              </Text>
+            </Stack.Item>
+          </Stack>
+        </Stack.Item>
+      </Stack>
+      <Stack.Item align="center">
         <Persona />
       </Stack.Item>
     </Stack>
