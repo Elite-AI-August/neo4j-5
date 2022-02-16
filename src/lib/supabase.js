eb96e70fee015ec1c77890511af8f1221048142c
CarollCheng <carollhwrd@gmail.com>
2022-02-15 19:52:35 -0600

    wip

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,3 +1,9 @@
+// use like so -
+// import { supabase } from '../lib/supabase'
+// const { data, error } = await supabase
+//   .from('posts')
+//   .select()
+
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
