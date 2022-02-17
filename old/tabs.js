b12b51751f1eeca0fba2a5fdb6dfff1f8cd53166
CarollCheng <carollhwrd@gmail.com>
2022-02-16 19:59:04 -0600

    cleanup

---
+++ old/tabs.js
@@ -0,0 +1,10 @@
+import { Pivot as Tab, PivotItem as TabItem } from '@fluentui/react' // actually a Tab control
+;<Tab>
+  <TabItem headerText="Table">
+    <br />
+  </TabItem>
+  <TabItem headerText="Scheduler">
+    <br />
+    <Scheduler />
+  </TabItem>
+</Tab>
--- src/pages/app.js
+++ src/pages/app.js
@@ -5,13 +5,9 @@
 
 // note: need to use lib-commonjs because otherwise nextjs complains
 import { ThemeProvider } from '@fluentui/react/lib-commonjs'
-// import { Pivot as Tab, PivotItem as TabItem } from '@fluentui/react' // actually a Tab control
-// import Scheduler from './views/scheduler/scheduler'
 import Table from '../panes/table'
 import Header from '../components/header'
 import { Sources } from '../sources'
-// import './app.css'
-// import { foo } from '../drivers/gcal'
 
 const appTheme = {
   palette: {
@@ -26,18 +22,8 @@ function App() {
   return (
     <ThemeProvider theme={appTheme}>
       <Header />
-      {/* . add Bar */}
-      {/* <Tab> */}
-      {/* <TabItem headerText="Table"> */}
-      {/* <br /> */}
+      {/* . put Bar here */}
       <Table sources={sources} />
-      {/* </TabItem> */}
-      {/* <TabItem headerText="Scheduler"> */}
-      {/* <br /> */}
-      {/* <Scheduler /> */}
-      {/* </TabItem> */}
-      {/* </Tab> */}
-      {/* <button onClick={foo}>calendar</button> */}
     </ThemeProvider>
   )
 }
